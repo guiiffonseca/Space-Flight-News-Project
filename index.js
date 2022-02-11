365f844085d7e4ea1984f93d23fd8bcf6c09081e
Guilherme Fonseca <guilherme.fonseca@paco.local>
2022-02-11 10:41:21 -0300

    Index com variaveis de ambiente

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ require("dotenv").config();
 
 const PORT = process.env.APP_PORT;
 const HOST = process.env.APP_HOST;
-const app = express();
 
+const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.status(200).send("Back-end Challenge 2021 🏅 - Space Flight News");
 });
+
+app.post("/articles");
 
 app.listen(PORT, HOST, () => console.log(`Running in http:${HOST}:${PORT}`));
